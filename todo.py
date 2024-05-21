721bcd01875fc7818e91568e08744d12c4c330c8
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-05-20 23:05:21 -0400

    Load and view functions created

--- todo.py
+++ todo.py
@@ -6,18 +6,33 @@ import os
 
 TODO_FILE = 'todo.txt'
 
+# Load
+def load_todos():
+    if os.path.exists(TODO_FILE):
+        with open(TODO_FILE, 'r') as file:
+            todos = [line.strip() for line in file.readlines()]
+    else:
+        todos = []
+    return todos
+
+# View
+def view_todos():
+    if todos:
+        print("\nTodo List:")
+        for idx, todo in enumerate(todos, start=1):
+            print(f"{idx}. {todo}")
+    else:
+        print("\nYour todo list is empty.")
+
 # Add
 def add_todo(todos):
     with open(TODO_FILE, 'w') as file:
         for todo in todos:
             file.write(todo + '\n')
+# Save
 
-# View
 
 # Delete
 
-# Save
-
-# Load
 
 # Create Interface
