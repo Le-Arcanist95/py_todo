1e18dcc7282a7c3fecad913b8f85dc72b118f53c
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-05-20 22:59:09 -0400

    Add function created

--- todo.py
+++ todo.py
@@ -2,7 +2,15 @@
 # Store items in list
 # List should be saved to file and loaded on application start
 
+import os
+
+TODO_FILE = 'todo.txt'
+
 # Add
+def add_todo(todos):
+    with open(TODO_FILE, 'w') as file:
+        for todo in todos:
+            file.write(todo + '\n')
 
 # View
 
