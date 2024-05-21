d9eba2b6636cd8f40df7185f2d2cf41b681d2646
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-05-20 22:56:13 -0400

    Outline comments

---
+++ todo.py
@@ -0,0 +1,15 @@
+# Allow users to add, view, delete, and save items
+# Store items in list
+# List should be saved to file and loaded on application start
+
+# Add
+
+# View
+
+# Delete
+
+# Save
+
+# Load
+
+# Create Interface
